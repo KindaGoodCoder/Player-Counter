5d6446dfa6b7eed7eeee11d32fe3d7a65a5d7eaf
Goodman <arafezazgaminglegend@gmail.com>
2022-10-31 13:59:54 +1100

    Finished version 2

--- playercounterv2.lua
+++ playercounterv2.lua
@@ -1,10 +1,10 @@
-scps = {5,6,10,11,12,13,14,15,16}
+scps = {5,6,10,11,12,13,14,15,16} --Team roles
 cd = {3,7,0,0,0,0,0,0,0}
 found = {1,2,4,8,9,0,0,0,0}
 
-text = {}
+text = {} --List for playertext
 
-function OnPlayerConnect() OnPlayerGetNewRole(); return -1 end
+function OnPlayerConnect() OnPlayerGetNewRole(); return -1 end --Update counter when people join game
 
 function playertext(plr,txt,y,clr) return createplayertext(plr,txt, screen_width, screen_height/y, clr, "Courier New Rus.ttf", 20) end
 
@@ -15,16 +15,16 @@ function OnPlayerGetNewRole()
 
             for y = 1, 9 do --Loop tho team roles lists. 9 is the max list
 
-                if pcall(function()
+                if pcall(function() --Select Case. Pcall() protects from errors. Will keep attempting select case until loop finishes
                     local select = {
-                    [0] = function() specs = specs + 1 end,
-                    [scps[y]] = function() scp = scp + 1 end,
-                    [found[y]] = function() secure = secure + 1 end,                        
-                    [cd[y]] = function() chaos = chaos + 1 end,
-                    [17] = function() end
+                    [0] = function() specs = specs + 1 end, --If spectator
+                    [scps[y]] = function() scp = scp + 1 end, --If SCP
+                    [found[y]] = function() secure = secure + 1 end,  --If Foundation                      
+                    [cd[y]] = function() chaos = chaos + 1 end, --If Chaos/CD
+                    [17] = function() end --Lobby role. Just not to break the system
                     }
-                    select[getplayertype(x)]()
-                end) then break end
+                    select[getplayertype(x)]() --Execute function depending on playertype
+                end) then break end --If the function doesn't raise an error, then move on
 
             end
             
@@ -37,7 +37,7 @@ function OnPlayerGetNewRole()
 
             if getplayertype(spec) == 0 then
 
-                screen_width = getplayermonitorwidth(spec)/45
+                screen_width = getplayermonitorwidth(spec)/45 --Get player screen size and maths is performed here and in the playertext() function
                 screen_height = getplayermonitorheight(spec)
                 text[1] = playertext(spec,"SCPs Remaining: ".. tostring(scp), 2.4,16711680) --red
                 text[2] = playertext(spec,"Security Remaining: ".. tostring(secure), 2.08, 255) --blue
@@ -48,5 +48,5 @@ function OnPlayerGetNewRole()
 
         end
     end
-    return -1
+    return -1 --Cause LUA breaks without return -1 according to Ne4to
 end
