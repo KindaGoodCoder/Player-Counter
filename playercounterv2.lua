532cc9540f8861b0cdf406aa1941dc3eac05f8f7
Goodman <arafezazgaminglegend@gmail.com>
2022-11-01 11:39:17 +1100

    Perfection

--- playercounterv2.lua
+++ playercounterv2.lua
@@ -4,12 +4,13 @@ found = {1,2,4,8,9,0,0,0,0}
 
 text = {} --List for playertext
 
+function OnScriptLoaded() print("PlayerCounter"); return -1 end
+
 function OnPlayerConnect() OnPlayerGetNewRole(); setplayertype(1,14); return -1 end --Update counter when people join game
 
 function playertext(plr,txt,y,clr) return createplayertext(plr,txt, screen_width, screen_height/y, clr, "Courier New Rus.ttf", 20) end
 
 function OnPlayerGetNewRole()
-    print("x")
     local scp,secure,chaos,specs = 0,0,0,0 --Reset counter
     for x = 1, 64 do --Loop tho each plr
         if isplayerconnected(x) == 1 then --connected
@@ -25,14 +26,12 @@ function OnPlayerGetNewRole()
                     [17] = function() end --Lobby role. Just not to break the system
                 }
                 --Execute function depending on playertype
-                if type(select[getplayertype(x)]) == "function" then select[getplayertype(x)]() then break end --If the function doesn't raise an error, then move on
+                if type(select[getplayertype(x)]) == "function" then select[getplayertype(x)](); break end --If the function doesn't raise an error, then move on
 
             end
             
         end
     end
-    print(specs)
-    print(scp)
     for spec = 1, 64 do
         if isplayerconnected(spec) == 1 then
 
