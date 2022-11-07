46520fd11031a64c91823ad8b7c35f75ef4f2a6b
Goodman <arafezazgaminglegend@gmail.com>
2022-11-07 18:08:50 +1100

    Bug workaround

--- playercounterv2.lua
+++ playercounterv2.lua
@@ -8,52 +8,57 @@ function OnScriptLoaded() print("PlayerCounter"); return -1 end
 
 function OnPlayerConnect() OnPlayerGetNewRole(); return -1 end --Update counter when people join game
 
-function playertext(plr,txt,y,clr) return createplayertext(plr,txt, screen_width, screen_height/y, clr, "Courier New Rus.ttf", 20) end
-
 function OnPlayerGetNewRole()
-    --setplayertype(1,14) --For debugging
-    local scp,secure,chaos,specs = 0,0,0,0 --Reset counter
-    for x = 1, 64 do --Loop tho each plr
-        if isplayerconnected(x) == 1 then --connected
+    update = function()
+        --setplayertype(1,14) --For debugging
+        local scp,secure,chaos,specs = 0,0,0,0 --Reset counter
+        for x = 1, 64 do --Loop tho each plr
+            if isplayerconnected(x) == 1 then --connected
 
-            for y = 1, 9 do --Loop tho team roles lists. 9 is the max list
+                for y = 1, 9 do --Loop tho team roles lists. 9 is the max list
 
-                --Will keep attempting select case until loop finishes
-                local select = {
-                    [0] = function() specs = specs + 1 end, --If spectator
-                    [scps[y]] = function() scp = scp + 1 end, --If SCP
-                    [found[y]] = function() secure = secure + 1 end,  --If Foundation                      
-                    [cd[y]] = function() chaos = chaos + 1 end, --If Chaos/CD
-                    [17] = function() end --Lobby role. Just not to break the system
-                }
-                --Execute function depending on playertype
-                if type(select[getplayertype(x)]) == "function" then select[getplayertype(x)](); break end --If the function doesn't raise an error, then move on
+                    --Will keep attempting select case until loop finishes
+                    local select = {
+                        [0] = function() specs = specs + 1 end, --If spectator
+                        [scps[y]] = function() scp = scp + 1 end, --If SCP
+                        [found[y]] = function() secure = secure + 1 end,  --If Foundation                      
+                        [cd[y]] = function() chaos = chaos + 1 end, --If Chaos/CD
+                        [17] = function() end --Lobby role. Just not to break the system
+                    }
+                    --Execute function depending on playertype
+                    if type(select[getplayertype(x)]) == "function" then select[getplayertype(x)](); break end --If the function doesn't raise an error, then move on
+                    
+                end
                 
             end
-            
         end
-    end
-    -- print(specs)
-    -- print(scp)
-    -- print(secure)
-    -- print(chaos)
-    for spec = 1, 64 do
-        if isplayerconnected(spec) == 1 then
+        -- print(specs)
+        -- print(scp)
+        -- print(secure)
+        -- print(chaos)
+        for spec = 1, 64 do
+            if isplayerconnected(spec) == 1 then
 
-            for x = 1, 4 do removeplayertext(spec,text[x]) end
+                for x = 1, 4 do removeplayertext(spec,text[x]) end
 
-            if getplayertype(spec) == 0 then
+                if getplayertype(spec) == 0 then
 
-                screen_width = getplayermonitorwidth(spec)/45 --Get player screen size and maths is performed here and in the playertext() function
-                screen_height = getplayermonitorheight(spec)
-                text[1] = playertext(spec,"SCPs Remaining: ".. scp, 2.4,16711680) --red
-                text[2] = playertext(spec,"Security Remaining: ".. secure, 2.08, 255) --blue
-                text[3] = playertext(spec,"CI/Class-D Remaining: ".. chaos, 1.84, 25600) --green
-                text[4] = playertext(spec,"Dead Players: ".. specs, 1.65, 255255255) --God knows
+                    local playertext = function(txt,y,clr) return createplayertext(spec,txt, screen_width, screen_height/y, clr, "Courier New Rus.ttf", 20) end
+
+                    screen_width = getplayermonitorwidth(spec)/45 --Get player screen size and maths is performed here and in the playertext() function
+                    screen_height = getplayermonitorheight(spec)
+                    text[1] = playertext("SCPs Remaining: " .. scp, 2.4,16711680) --red
+                    text[2] = playertext("Security Remaining: " .. secure, 2.08, 255) --blue
+                    text[3] = playertext("CI/Class-D Remaining: " .. chaos, 1.84, 25600) --green
+                    text[4] = playertext("Dead Players: " .. specs, 1.65, 255255255) --God knows
+
+                end
 
             end
-
         end
+        return -1 --Cause LUA breaks without return -1 according to Ne4to
     end
-    return -1 --Cause LUA breaks without return -1 according to Ne4to
+    update()
+    createtimer("update",1500,0) --Cause GUI sometimes bugs out
+    return -1
 end
