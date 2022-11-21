f74f894291ca3458958ab941b69f28304dd57a5d
Goodman <arafezazgaminglegend@gmail.com>
2022-11-22 09:31:24 +1100

    a

--- PlayerTxt.gs
+++ PlayerTxt.gs
@@ -1,24 +1,24 @@
 #PLAYERSCRIPT
 
-global counter
+global counter = [4,SE_INT]
 
 public def OnReceiveRawPacket(data)
-    PeekByte(data)
-    counter = [4,SE_INT]
-    data = "64|64|64|64"
-
-    for x = 0; x < 3; x++
-        pos = Instr(data,"|",1)        
-        counter[x] = Left(data,pos-1)
-        data = Right(data,len data - pos)
-    end
-    
-    counter[3] = data
-
+    counter[0] = peekint(data,0)
+    counter[1] = peekint(data,1)
+    counter[2] = peekint(data,2)
+    counter[3] = peekint(data,3)
+    freebank(data)
 end
 
 public def OnUpdate()
-    text(480,280,data[1],0,0)
+    local width = getmonitorwidth()/45
+    local height = getmonitorheight()
+
+    text(width,height/2.4,counter[0],0,0)
+    text(width,height/2.08,counter[1],0,0)
+    text(width,height/1.84,counter[2],0,0)
+    text(width,height/1.65,counter[3],0,0)
+
     if MouseHit2() then
         SetGameMessage("Misery",1)
     end
--- playercounterv2.lua
+++ playercounterv2.lua
@@ -6,81 +6,48 @@ text = {} --List for playertext
 
 function OnScriptLoaded() --Check if script loaded
     print("PlayerCounter")
-    pain = createbank(64)
-    pokebyte(pain,0,string.byte("6"))
-    pokebyte(pain,1,string.byte("|"))
-    print(peekbyte(pain,0))
-    print(peekbyte(pain,1))
-    -- print(chr(peekbyte(pain,0)))
     
-    return -1
-end
-
-function OnPlayerConsole()
-    print(x)
-    sendscript(11,"Player-Counter/PlayerTxt.gsc","Test/playercounter.gsc")
-    sendrawpacket(11,x)
     return -1
 end
 
 function OnPlayerConnect(plr)
     OnPlayerGetNewRole()
+    sendscript(plr,"Player-Counter/PlayerTxt.gsc","Test/playercounter.gsc")    
     -- if plr == 11 then        
     -- end
     return -1 --Update counter when people join game
 end
 
 function OnPlayerGetNewRole()
+    --setplayertype(1,14) --For debugging
+    local scp,secure,chaos,specs = 0,0,0,0 --Reset counter
+    for x = 1, 64 do --Loop tho each plr
+        if isplayerconnected(x) == 1 then --connected
 
-    update = function()
-        --setplayertype(1,14) --For debugging
-        local scp,secure,chaos,specs = 0,0,0,0 --Reset counter
-        for x = 1, 64 do --Loop tho each plr
-            if isplayerconnected(x) == 1 then --connected
+            for y = 1, 9 do --Loop tho team roles lists. 9 is the max list
 
-                for y = 1, 9 do --Loop tho team roles lists. 9 is the max list
-
-                    --Will keep attempting select case until loop finishes
-                    local select = {
-                        [0] = function() specs = specs + 1 end, --If spectator
-                        [scps[y]] = function() scp = scp + 1 end, --If SCP
-                        [found[y]] = function() secure = secure + 1 end,  --If Foundation                      
-                        [cd[y]] = function() chaos = chaos + 1 end, --If Chaos/CD
-                        [17] = function() end --Lobby role. Just not to break the system
-                    }
-                    --Execute function depending on playertype
-                    if type(select[getplayertype(x)]) == "function" then select[getplayertype(x)](); break end --If the function doesn't raise an error, then move on
-                    
-                end
+                --Will keep attempting select case until loop finishes
+                local select = {
+                    [0] = function() specs = specs + 1 end, --If spectator
+                    [scps[y]] = function() scp = scp + 1 end, --If SCP
+                    [found[y]] = function() secure = secure + 1 end,  --If Foundation                      
+                    [cd[y]] = function() chaos = chaos + 1 end, --If Chaos/CD
+                    [17] = function() end --Lobby role. Just not to break the system
+                }
+                --Execute function depending on playertype
+                if type(select[getplayertype(x)]) == "function" then select[getplayertype(x)](); break end --If the function doesn't raise an error, then move on
                 
             end
+            
         end
-
-        for spec = 1, 64 do
-            if isplayerconnected(spec) == 1 then
-
-                for x = 1, 4 do removeplayertext(spec,text[x]) end
-
-                if getplayertype(spec) == 0 then
-
-                    local playertext = function(txt,y,clr) return createplayertext(spec,txt, screen_width, screen_height/y, clr, "Courier New Rus.ttf", 20) end
-
-                    screen_width = getplayermonitorwidth(spec)/45 --Get player screen size and maths is performed here and in the playertext() function
-                    screen_height = getplayermonitorheight(spec)
-                    text[1] = playertext("SCPs Remaining: " .. scp, 2.4,16711680) --red
-                    text[2] = playertext("Security Remaining: " .. secure, 2.08, 255) --blue
-                    text[3] = playertext("CI/Class-D Remaining: " .. chaos, 1.84, 25600) --green
-                    text[4] = playertext("Dead Players: " .. specs, 1.65, 255255255) --God knows
-
-                end
-
-            end
-        end
-        return -1 --Cause LUA breaks without return -1 according to Ne4to
     end
 
-    update()
-    createtimer("update",1500,0) --Cause GUI sometimes bugs out
-    createtimer("update",3000,0)
+    data = createbank(7)
+    for x,v in ipairs({scp,secure,chaos,specs}) do
+        pokeint(data,x-1,v)
+    end
+    print(peekint(data,0))
+    if isplayerconnected(11) == 1 then sendrawpacket(11,data) end    
+
     return -1
 end
